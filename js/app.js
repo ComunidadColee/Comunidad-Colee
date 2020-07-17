3ce78b810dfb34bc97eb9e6b636fc6e05f5937e6
ComunidadColee <coleecomunidad@gmail.com>
2020-07-16 21:35:52 -0500

    buscador funcionando

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 inputSearch = document.getElementById("inputSearch");
+box_search = document.getElementById("box-search");
 
-function buscador() {
-    filtro = inputSearch.value.toUpperCase();
+document.getElementById("inputSearch").addEventListener("keyup", buscador_interno);
+
+function buscador_interno(){
+
+
+    filter = inputSearch.value.toUpperCase();
+    li = box_search.getElementsByTagName("li");
+
+    //Recorriendo elementos a filtrar mediante los "li"
+    
+    for (i = 0; i < li.length; i++){
+
+        a = li[i].getElementsByTagName("a")[0];
+        textValue = a.textContent || a.innerText;
+
+        if(textValue.toUpperCase().indexOf(filter) > -1){
+
+            li[i].style.display = "";
+            box_search.style.display = "block";
+
+            if (inputSearch.value === ""){
+                box_search.style.display = "none";
+            }
+
+        }else{
+            li[i].style.display = "none";
+        }
+
+    }
 }
